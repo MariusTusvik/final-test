7c47ffc80322731b4c382a8c2b6d7ee6ddc3977a
AlmightyDuckman <115139566+MariusTusvik@users.noreply.github.com>
2023-04-13 16:00:35 +0200

    Navbar fixes

--- src/app/components/drop-down/drop-down.component.ts
+++ src/app/components/drop-down/drop-down.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, HostListener, ElementRef } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
@@ -7,5 +7,23 @@ import { MatMenuTrigger } from '@angular/material/menu';
   styleUrls: ['./drop-down.component.scss'],
 })
 export class DropDownComponent {
-  menuTrigger!: MatMenuTrigger;
+  @Input() title: string = '';
+  @Input() menuItems: string[] = [];
+
+  isOpen = false;
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+    console.log('toggleMenu');
+  }
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('document:click', ['$event.target'])
+  public onDocumentClick(targetElement: HTMLElement): void {
+    const clickedInside = this.el.nativeElement.contains(targetElement);
+    if (!clickedInside) {
+      this.isOpen = false;
+    }
+  }
 }
