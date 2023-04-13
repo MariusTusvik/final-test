ac54576bd9ecece409678151f8bdfb7535bee4c6
AlmightyDuckman <115139566+MariusTusvik@users.noreply.github.com>
2023-04-13 16:34:44 +0200

    formatting, community section, ai platform section, landing fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         cyan: "#097a80",
         teal: "#173c3e",
         gray: "#24282c",
+        offGray: "#3d4044",
         lightgray: "#d7d8d7",
         offWhite: "#dedede",
       },
